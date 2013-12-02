2dbe9d08bc02b5c00a16c8a04725244d32d9f501
Timm <kaspth@gmail.com>
2013-12-02 17:57:08 +0100

    Wrapped products in a link.

--- includes/_product.php
+++ includes/_product.php
@@ -1,7 +1,9 @@
 <article class="product">
-  <img src="<?php echo $product["image_url"]; ?>" alt="<?php echo $product["name"]; ?>">
-  <div class="background">
-    <h3><?php echo $product["name"]; ?></h3>
-    <em data-button-replace><?php echo $product["price"]; ?> kr.</em>
-  </div>
+  <a href="product.php?id=<?php echo $product["id"]; ?>">
+    <img src="<?php echo $product["image_url"]; ?>" alt="<?php echo $product["name"]; ?>">
+    <div class="background">
+      <h3><?php echo $product["name"]; ?></h3>
+      <em data-button-replace><?php echo $product["price"]; ?> kr.</em>
+    </div>
+  </a>
 </article>
