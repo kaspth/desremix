df9e300a056c17c7a2f4a8390b36e6113ef33a70
Timm <kaspth@gmail.com>
2013-12-03 15:33:12 +0100

    Changed data-button-replace to be data-hover-replace. Added add to cart button.

--- includes/_product.php
+++ includes/_product.php
@@ -3,7 +3,8 @@
     <img src="<?php echo $product["image_url"]; ?>" alt="<?php echo $product["name"]; ?>">
     <div class="background">
       <h3><?php echo $product["name"]; ?></h3>
-      <em data-button-replace><?php echo $product["price"]; ?> kr.</em>
+      <em data-hover-replace><?php echo $product["price"]; ?> kr.</em>
+      <button data-hover-replacement data-add-to-cart>Læg i kurv</button>
     </div>
   </a>
 </article>
