55d52edf645dc8fd67577bb6c85c6e2be06f2336
Timm <kaspth@gmail.com>
2013-12-03 17:08:16 +0100

    Outputting total_price in line items partial.

--- includes/_line_item.php
+++ includes/_line_item.php
@@ -1,5 +1,5 @@
 <tr>
   <td><?php echo $line_item['count']; ?></td>
   <td><?php echo $line_item['name']; ?></td>
-  <td><?php echo $line_item['price']; ?></td>
+  <td><?php echo $line_item['total_price']; ?></td>
 </tr>
