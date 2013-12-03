1cbd8c7f32f70202e01a47f3a7dab2c5f4a83e09
Timm <kaspth@gmail.com>
2013-12-03 15:47:58 +0100

    Made add to cart button fade in and out.

--- assets/application.js
+++ assets/application.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+  $('[data-replacement]').hide();
 
+  $('[data-hover-replace]').on('mouseenter', function() {
+    fadeAndRevealOn($(this), '[data-replace]', '[data-replacement]');
+  }).on('mouseleave', function() {
+    fadeAndRevealOn($(this), '[data-replacement]', '[data-replace]');
+  });
+
+  var fadeAndRevealOn = function(el, fadeSelector, revealSelector, speed) {
+    if (!speed) speed = 200;
+    el.find(fadeSelector).fadeOut(speed, function() {
+      el.find(revealSelector).fadeIn(speed);
+    });
+  };
 });
--- includes/_product.php
+++ includes/_product.php
@@ -1,10 +1,10 @@
-<article class="product">
+<article class="product" data-hover-replace>
   <a href="product.php?id=<?php echo $product["id"]; ?>">
     <img src="<?php echo $product["image_url"]; ?>" alt="<?php echo $product["name"]; ?>">
     <div class="background">
       <h3><?php echo $product["name"]; ?></h3>
-      <em data-hover-replace><?php echo $product["price"]; ?> kr.</em>
-      <button data-hover-replacement data-add-to-cart>Læg i kurv</button>
+      <em data-replace><?php echo $product["price"]; ?> kr.</em>
+      <button data-replacement data-add-to-cart>Læg i kurv</button>
     </div>
   </a>
 </article>
