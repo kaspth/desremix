24ef716d3c86fc8506b8cf396a44abf53b72dee3
Timm <kaspth@gmail.com>
2013-12-03 17:06:24 +0100

    Copied cart header to sidebar, when cart is empty.

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,6 +2,9 @@
   <section class="cart">
 
     <?php echo render($cart, 'cart', function() { ?>
+      <header>
+        <h1>Your Cart</h1>
+      </header>
       <h3>Your cart is empty.</h3>
     <?php }); ?>
 
