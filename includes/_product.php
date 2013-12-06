a19b4914656c252ac4cb46e4d54f10a30de9297b
Timm <kaspth@gmail.com>
2013-12-06 11:25:57 +0100

    Fixed validation error: add to cart buttons are not inside a tag. This means you can't click on the banner and go to a product.

--- includes/_product.php
+++ includes/_product.php
@@ -3,10 +3,10 @@
 >
   <a href="product.php?id=<?php echo $product["id"]; ?>">
     <img src="<?php echo $product["image_url"]; ?>" alt="<?php echo $product["name"]; ?>">
-    <div class="background">
-      <button data-hover-show data-add-to-cart>Læg i kurv</button>
-      <h3><?php echo $product["name"]; ?></h3>
-      <em><?php echo $product["price"]; ?> kr.</em>
-    </div>
   </a>
+  <div class="background">
+    <button data-hover-show data-add-to-cart>Læg i kurv</button>
+    <h3><?php echo $product["name"]; ?></h3>
+    <em><?php echo $product["price"]; ?> kr.</em>
+  </div>
 </article>
