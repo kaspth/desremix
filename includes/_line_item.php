c7e232d9dbae0fb1494cb60ad8f21562b35522f3
Timm <kaspth@gmail.com>
2013-12-08 12:38:17 +0100

    Fixed broken incremental line item updates. Forgot to add data-product-id to a line item.

--- includes/_line_item.php
+++ includes/_line_item.php
@@ -1,4 +1,4 @@
-<tr>
+<tr data-product-id="<?php echo $line_item['product_id']; ?>">
   <td><?php echo $line_item['count']; ?></td>
   <td><?php echo $line_item['name']; ?></td>
   <td>DKK <?php echo $line_item['total_price']; ?></td>
