3c5cf2298399a3087815d13314e3912de2ce2e3e
Timm <kaspth@gmail.com>
2013-12-14 14:38:56 +0100

    Simplified products_helper.php a ton.

--- helpers/products_helper.php
+++ helpers/products_helper.php
@@ -12,27 +12,12 @@ function fetch_products() {
 }
 
 function fetch_product_by_id($id) {
-  return find_product_by("id", $id);
-}
-
-function find_product_by($key, $value) {
-  $found_product = null;
-  each_product(function($product) use ($key, $value, &$found_product) {
-    if ($product[$key] == $value) {
-      $found_product = $product;
-      return true;
-    }
-  });
-  return $found_product;
-}
-
-function each_product($operation) {
   $products = fetch_products();
   if (!isset($products)) return;
 
   foreach ($products as $product)
-    if ($operation($product))
-      break;
+    if ($product['id'] == $id)
+      return $product;
 }
 
 function products_path() {
